5b880b2efbadc06267477f53364845fa3f316909
mateoleont <mateolt505@gmail.com>
2024-02-18 14:06:30 -0500

    crud listo en el back

--- src/app/api/tasks/[id]/route.js
+++ src/app/api/tasks/[id]/route.js
@@ -2,46 +2,68 @@ import { NextResponse } from "next/server"
 import { prisma } from "@/libs/prisma"
 
 const GET = async (request, {params}) => {
-    const id = parseInt(params.id)
-    const task = await prisma.task.findUnique({
-        where: {
-            id : id
-        }
-    })
-    return NextResponse.json({
-        type: `Getting task ${params.id}`,
-        data: task ? task : 'Task not found'
-    })   
+    try {
+        const id = parseInt(params.id)
+        const task = await prisma.task.findUnique({
+            where: {
+                id : id
+            }
+        })
+        return NextResponse.json({
+            type: `Getting task ${params.id}`,
+            data: task ? task : 'Task not found'
+        })      
+    } 
+    catch (error) {
+        return NextResponse.json({
+            type: `Getting task ${params.id}`,
+            error: error.message
+        })   
+    }
 }
 
 const PUT = async (request, {params}) => {
-    const id = parseInt(params.id)
-    const {title, description} = await request.json()
-    const updateTask = await prisma.task.update({
-        data: {
-            title, description
-        },
-        where: {
-            id: id
-        }
-    })
-    return NextResponse.json({
-        type: `Updating task ${params.id}`,
-        data: updateTask ? updateTask : 'Task not found'
-    })   
+    try {
+        const id = parseInt(params.id)
+        const data = await request.json()
+        const updateTask = await prisma.task.update({
+            data: data,
+            where: {
+                id: id
+            }
+        })
+        return NextResponse.json({
+            type: `Updating task ${params.id}`,
+            data: updateTask
+        }) 
+    } 
+    catch (error) {
+        return NextResponse.json({
+            type: `Updating task ${params.id}`,
+            error: error.message
+        }) 
+    }
 }
 
 const DELETE = async (request, {params}) => {
-    const id = parseInt(params.id)
-    const taskRemove = await prisma.task.delete({
-        where: {
-            id: id
-        }
-    })
-    return NextResponse.json({
-        type: `Deleting task ${params.id}`,
-        data: taskRemove ? taskRemove : "Task not found"
-    })   
+    try {
+        const id = parseInt(params.id)
+        const taskRemove = await prisma.task.delete({
+            where: {
+                id: id
+            }
+        })
+        return NextResponse.json({
+            type: `Deleting task ${params.id}`,
+            data: taskRemove 
+        })   
+    } 
+    catch (error) {
+        return NextResponse.json({
+            type: `Deleting task ${params.id}`,
+            error: error.message
+        })   
+    }
 }
 
 export {GET, PUT, DELETE}
--- src/app/api/tasks/route.js
+++ src/app/api/tasks/route.js
@@ -2,26 +2,38 @@ const { NextResponse } = require("next/server")
 import { prisma } from "@/libs/prisma"
 
 const GET = async () => {
-    const tasks = await prisma.task.findMany()
-
-    return NextResponse.json({
-        type: `Getting All Tasks`,
-        data: tasks.length > 0 ? tasks : 'There are no tasks'
-    })
+    try {
+        const tasks = await prisma.task.findMany()
+        return NextResponse.json({
+            type: `Getting All Tasks`,
+            data: tasks.length > 0 ? tasks : 'There are no tasks'
+        })   
+    } 
+    catch (error) {
+        return NextResponse.json({
+            type: `Getting All Tasks`,
+            error: error.message
+        })   
+    }
 }
 
 const POST = async (request) => {
-    const {title, description} = await request.json() 
-    const newTask = await prisma.task.create({
-        data: {
-            title, description
-        }
-    })
-
-    return NextResponse.json({
-        type: `Creating Tasks`,
-        data: newTask ? newTask : 'Error creating tasks'
-    })
+    try {
+        const data = await request.json() 
+        const newTask = await prisma.task.create({
+            data: data
+        })
+    
+        return NextResponse.json({
+            type: `Creating Tasks`,
+            data: newTask ? newTask : 'Error creating tasks'
+        })
+    } catch (error) {
+        return NextResponse.json({
+            type: `Creating Tasks`,
+            error: error.message
+        })
+    }
 }
 
 export {GET, POST}
