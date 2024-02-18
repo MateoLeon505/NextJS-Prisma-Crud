91b453c64bd7509e8d24e9b08d44ef6b2a68f154
mateoleont <mateolt505@gmail.com>
2024-02-18 00:41:19 -0500

    estructura en definición

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,8 @@
 const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-    </main>
+    <div className="">
+      Tasks
+    </div>
   );
 }
 
