cb9907181dd739d8ef61d1001586a9685dad11bf
mateoleont <mateolt505@gmail.com>
2024-02-18 15:47:43 -0500

    modularizado taskcard

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+import TakCard from "@/components/taskCard"
 
 const loadTasks = async () => {
   const res = await fetch('http://localhost:3000/api/tasks')
@@ -12,11 +13,7 @@ const Home = async () => {
     <section className="container mx-auto py-10 px-5">
       <div className="grid grid-cols-3 gap-3">
         {tasks.map((task) => (
-          <div key={task.title} className="bg-slate-900 p-3 hover:bg-slate-600 hover:cursor-pointer">
-            <h3 className="font-bold text-2xl mb-2">{task.title}</h3>
-            <p>{task.description}</p>
-            <p>{new Date(task.createdAt).toLocaleDateString()}</p>
-          </div>
+          <TakCard key={task.title} task={task}/>
         ))}
       </div>
     </section>
