6d6a8952707aab04b5ad819d2a9e4b327d62f8b7
mateoleont <mateolt505@gmail.com>
2024-02-18 21:08:22 -0500

    diseño y ricons

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section className="container m-auto h-[calc(100vh-7rem)] justify-center ">
+        <h3 className="text-5xl font-bold my-5 text-center">About</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus
+          quidem nulla tenetur a qui accusamus consequuntur natus quaerat, sequi
+          laudantium dolore magnam! Tenetur maiores alias saepe. Recusandae
+          expedita ea at iusto, architecto consequatur id neque perspiciatis
+          aut, corporis eligendi deserunt beatae sapiente dolore quo aperiam
+          voluptas debitis vel labore maiores numquam voluptatem aliquam? Hic
+          quod ab reprehenderit necessitatibus nihil quia adipisci fuga,
+          pariatur sit repellat. Ex, fugiat dolor perferendis iusto harum quae
+          quia. Reiciendis consequuntur laboriosam officia, obcaecati tempore ad
+          voluptates quae libero sed! Dolores inventore tenetur earum vero,
+          iusto, delectus veniam et maxime maiores recusandae amet? Alias,
+          nesciunt magnam.
+        </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 
 const NotFound = () => {
   return (
     <section className='flex h-[calc(100vh-7rem)] justify-center items-center'>
-        <div>
+        <div className='text-center'>
             <h1 className='text-5xl font-bold'>Not Found</h1>
+            <Link href='/' className='text-slate-300 mt-5 text-2xl hover:text-slate-100'>Return to Tasks</Link>
         </div>
     </section>
   )
