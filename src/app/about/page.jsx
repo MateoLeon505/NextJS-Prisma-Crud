41947baf0ba107d2ad32d752453e66b4e9d76b63
mateoleont <mateolt505@gmail.com>
2024-02-18 23:52:35 -0500

    resp gral

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,17 +1,30 @@
 import React from "react";
+import Link from "next/link";
+import { FaLinkedin } from "react-icons/fa";
+import { FaSquareGithub } from "react-icons/fa6";
 
 const About = () => {
   return (
-    <section className="container h-[calc(100vh-7rem)] justify-center xs:px-5 md:px-20 py-10">
+    <section className="container mx-auto px-5 md:px-20 py-10">
         <h3 className="text-5xl font-bold my-5 text-center">About</h3>
         <div className="xs:text-xs sm:text-xl" >
           <p className="mb-7">
             I´m Mateo León. This project is a CRUD (Create, Read, Update, Delete) developed to manage tasks.
             This project uses Next.js, Rest API, Prisma, React icons and an SQLite database to store the tasks.
+            I developed it with the next de fazt code course.
           </p>
-          <p>
+          <p className="mb-15">
             The app allows users to add new tasks, edit details of existing tasks, and delete tasks.
           </p>
+          <h3 className="text-3xl font-bold my-5 text-center">Find me on</h3>
+          <div className="container flex row justify-center gap-10">
+            <Link href='https://github.com/MateoLeon505' target="blank">
+              <FaLinkedin className="text-slate-400 text-5xl hover:text-white hover:scale-110 "/>
+            </Link>
+            <Link href='https://github.com/MateoLeon505 target="blank"'>
+              <FaSquareGithub className="text-slate-400 text-5xl hover:text-white hover:scale-110"/>
+            </Link>
+          </div>
         </div>
     </section>
   );
