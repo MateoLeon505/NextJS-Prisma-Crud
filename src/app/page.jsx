00d990d7b9b0b90b69fbe613f892cd85f245188e
mateoleont <mateolt505@gmail.com>
2024-02-27 18:38:39 -0500

    last changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,20 @@
 "use client"
+import React, { useState, useEffect } from 'react'
 import TakCard from "@/components/taskCard"
 
-const loadTasks = async () => {
-  const res = await fetch('http://localhost:3000/api/tasks', {
-    method: 'GET',
-    cache: 'no-store',
-  })
-  let data = await res.json()
-  data = await data.data
-  console.log(data);
-  return data
-}
+const Home = () => {
+  const [ tasks, setTasks ] = useState([])
 
-const Home = async () => {
-  const tasks = await loadTasks()
-  console.log(tasks);
+  useEffect(() => {
+    fetch('http://localhost:3000/api/tasks', {
+      method: 'GET',
+      cache: 'no-store',
+    })
+    .then(res => res.json())
+    .then(data => data = data.data) 
+    .then(data => setTasks(data)) 
+  })
+
   return (
     <section className="container mx-auto py-10 px-5">
       <div className="grid xs:grid-cols-1 sm:grid-cols-2  md:grid-cols-3 gap-3">
